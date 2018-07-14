fb4e61895973bf32bed5dbb8841802962f290f18
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-14 04:38:59 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -3,7 +3,7 @@ def fourth_challenge
      :montague => {
         :patriarch => {name: "Lord Montague", age: "53"},
         :matriarch => {name: "Lady Montague", age: "54"},
-        :hero => {name: "Romeo", age: "15", status: "alive"},
+        :hero => {name: "Romeo", age: "15", status: "dead"},
         :hero_friends => [
            {name: "Benvolio", age: "17", attitude: "worried"},
            {name: "Mercutio", age: "18", attitude: "hot-headed"}
@@ -12,7 +12,7 @@ def fourth_challenge
      :capulet => {
         :patriarch => {name: "Lord Capulet", age: "50"},
         :matriarch => {name: "Lady Capulet", age: "51"},
-        :heroine => {name: "Juliet", age: "15", status: "alive"},
+        :heroine => {name: "Juliet", age: "15", status: "dead""},
         :heroine_friends => [
             {name: "Steven", age: "30", attitude: "confused"},
             {name: "Nurse", age: "44", attitude: "worried"}
